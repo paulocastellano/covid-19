33830e05c99820f145d819b50e900cadf7147233
Paulo Castellano <paulofcastellano@gmail.com>
2020-04-11 20:34:26 -0300

    ajustando header para o botao do github parecer no mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,32 +21,8 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Covid 19
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="btn btn-sm btn-light" href="https://github.com/paulocastellano/covid-19" target="_blank">
-                                Github
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('layouts.header')
 
         <main class="py-4">
             @yield('content')
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            Covid 19
+        </a>
+
+
+        <div id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="btn btn-sm btn-light" href="https://github.com/paulocastellano/covid-19" target="_blank">
+                        Github
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>

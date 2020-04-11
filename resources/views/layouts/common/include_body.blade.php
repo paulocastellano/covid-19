e1a5955e9cd1ea8c33edaef6b2e24a755d25d6a7
Paulo Castellano <paulofcastellano@gmail.com>
2020-04-11 20:49:00 -0300

    ajustando gtm que precisa estar em string

--- resources/views/layouts/common/include_body.blade.php
+++ resources/views/layouts/common/include_body.blade.php
@@ -1,6 +1,6 @@
 @if(env('APP_ENV') == 'production')
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('GOOGLE_GTM_CODE') }}"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{env('GOOGLE_GTM_CODE')}}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 @endif

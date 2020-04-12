dd6ae80e730c5336ae9d6e6290694c5a30d7c939
Paulo Castellano <paulofcastellano@gmail.com>
2020-04-12 14:33:35 -0300

    pequeno ajuste no mobile

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,15 +5,12 @@
                 Covid-19
             </a>
             <div class="d-flex order-lg-2 ml-auto">
-                <div class="nav-item d-none d-md-flex">
+                <div class="nav-item">
                     <a href="https://github.com/paulocastellano/covid-19" class="btn btn-github btn-sm" target="_blank">
                         <i class="fa fa-github mr-2"></i>Github
                     </a>
                 </div>
             </div>
-            <a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse" data-target="#headerMenuCollapse">
-                <span class="header-toggler-icon"></span>
-            </a>
         </div>
     </div>
 </div>
